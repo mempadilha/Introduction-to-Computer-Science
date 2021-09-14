2bef7dc52b913d94a65211f1d22d9732e2aafdb5
mempadilha <mmuller@chapelschool.com>
2021-09-14 19:03:42 -0300

    Lesson 6- first video

---
+++ intro to C#/Assets/Scripts/IntroToC/Lesson 6/ButtonTest.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTest : MonoBehaviour
+{
+  public void Click()
+    {
+        Debug.Log("Ouch! you clicked me");
+    }
+}
